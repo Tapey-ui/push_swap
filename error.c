1cf8cd52454cc6b13fe646f194656ee83319e99b
Ng Wai Keat <nwai-kea@u90z04s06.42kl.edu.my>
2023-03-28 22:31:35 +0800

    makefile time

--- error.c
+++ error.c
@@ -6,16 +6,16 @@
 /*   By: nwai-kea <nwai-kea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 19:09:52 by nwai-kea          #+#    #+#             */
-/*   Updated: 2023/03/28 22:19:36 by nwai-kea         ###   ########.fr       */
+/*   Updated: 2023/03/28 22:23:15 by nwai-kea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+// system("leaks -q push_swap");
 void	ft_error(int i)
 {
 	write(1, "Error\n", 6);
-	system("leaks -q push_swap");
 	exit(i);
 }
 
